deb7047d56eeb2b9e2ccd0c73456a26a5da47d34
StyloGey <stylogey@gmail.com>
2020-12-17 21:55:25 +0100

    ui: build: paths: Log make
     * Required for out of tree QCACLD building, also 5.4
       kernel scripts.
    
    Change-Id: Ie871e8a27968416080f0c698a7ea057a0588fb1d

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -110,6 +110,7 @@ var Configuration = map[string]PathConfig{
 	// CAF
 	"cpio":    Allowed,
 	"nproc":   Allowed,
+	"make":    Log,
 
 	// Host toolchain is removed. In-tree toolchain should be used instead.
 	// GCC also can't find cc1 with this implementation.
